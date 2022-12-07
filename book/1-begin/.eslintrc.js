473e6ed218a56ddeac9399c6d6dc66df56f2c313
Aleksei Saunders <alekseisaunders@gmail.com>
2022-12-07 14:18:05 -0700

    inital commit after package install

--- book/1-begin/.eslintrc.js
+++ book/1-begin/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
+        endOfLine: 'auto',
         singleQuote: true,
         trailingComma: 'all',
         arrowParens: 'always',
